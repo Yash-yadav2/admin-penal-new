5eb97ff4dd31f83fe2d61b064ee95e8ea393c962
Yash-yadav2 <yashyadav20023@gmail.com>
2025-04-04 22:18:47 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:5000/api';  // Backend API URL
+axios.defaults.baseURL = 'https://backend-new-u421.onrender.com:5000/api';  // Backend API URL
 axios.defaults.withCredentials = true;  // Enable cookies & sessions
 
 export default axios;
